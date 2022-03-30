f5f00e6afca8d60ef41af6f9d2b73f38eb552a40
Gilly <gilly.huga@yahoo.co.id>
2022-03-31 00:33:32 +0700

    exercise module-3-session-2

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,48 +1,46 @@
-import { Component } from "react";
+import { useEffect, useState } from "react";
 import GifComponent from "../../component/Gif";
 import './style.css';
 
-class Search extends Component {
-    state = {
-        gifs: [],
-        text: ""
-    }
+function Search() {
 
-    handleInput = (e) => this.setState({ text: e.target.value });
+    const [gifs, setGifs] = useState([])
+    const [text, setText] = useState("")
 
-    getGifs = async () => {
+    const handleInput = (e) => setText(e.target.value);
+
+    useEffect(() => {
+        getGifs()
+    },[]);
+
+    const getGifs = async () => {
         const gifs = await fetch(
-            `http://api.giphy.com/v1/gifs/search?q=${this.state.text}&api_key=${process.env.REACT_APP_GIPHY_KEY}&limit=12`
+            `http://api.giphy.com/v1/gifs/search?q=${text}&api_key=${process.env.REACT_APP_GIPHY_KEY}&limit=12`
         ).then((response) => response.json());
-        this.setState({ gifs: gifs.data });
+        setGifs(gifs.data)
     };
 
-    render() {
-        const { gifs } = this.state;
-
-        console.log(gifs)
-        return (
-            <div>
-                <input onChange={this.handleInput} />
-                <button onClick={this.getGifs}>Button</button>
-                <div className="container">
-                    {gifs.length ?
-                        gifs.filter(e => e.rating === 'g').map(e =>
-                            <GifComponent
-                                key={e.id}
-                                title={e.title}
-                                image={e.images.fixed_width.url}
-                                rating={e.rating}
-                            />
-                        )
-                        :
-                        null
-                    }
-                </div>
+    return (
+        <div>
+            <input onChange={handleInput} />
+            <button onClick={getGifs}>Button</button>
+            <div className="container">
+                {gifs.length ?
+                    gifs.filter(e => e.rating === 'g').map(e =>
+                        <GifComponent
+                            key={e.id}
+                            title={e.title}
+                            image={e.images.fixed_width.url}
+                            rating={e.rating}
+                        />
+                    )
+                    :
+                    null
+                }
             </div>
+        </div>
 
-        );
-    }
+    );
 }
 
 export default Search;
